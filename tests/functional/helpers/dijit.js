92794938f31040a6c88424ad63f6144f9e6c4882
dylans <dylan@dojotoolkit.org>
2014-09-06 06:53:37 -0700

    incremental updates to tests, still need to get the mock/stub for dojo/when to work correctly

--- tests/functional/helpers/dijit.js
+++ tests/functional/helpers/dijit.js
@@ -17,17 +17,31 @@ define([
 				});
 			},
 
-			'basic test': function () {
+			'.getProperty': function () {
 				return command
 					.get(require.toUrl('../data/dijit.html'))
-					.then(dijit.get('foo'))
-					.then(function (widget) {
-						assert.isFunction(widget.get, 'widget.get is a function');
-						assert.strictEqual(widget.get('a'), 1, 'widget.get(a) === 1');
-					})
+					.setFindTimeout(4000)
+					.setPageLoadTimeout(4000)
+					.setExecuteAsyncTimeout(4000)
 					.then(dijit.getProperty('foo', 'c'))
 					.then(function (c) {
 						assert.strictEqual(c, 3, 'widget.getProperty(foo, c) === 3');
+					})
+			},
+
+			'.get': function () {
+				return command
+					.get(require.toUrl('../data/dijit.html'))
+					.setFindTimeout(4000)
+					.setPageLoadTimeout(4000)
+					.setExecuteAsyncTimeout(4000)
+					.then(dijit.get('foo'))
+					.then(function (widget) {
+						assert.isFunction(widget.get, 'widget.get is a function');
+						return widget.get('a');
+					})
+					.then(function (a) {
+						assert.strictEqual(a, 1, 'widget.get(a) === 1');
 					});
 			}
 		};
