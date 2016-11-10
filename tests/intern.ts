29fd9797c58c08e6848f19f74e230ca6fda2ba39
Nick Nisi <nick@nisi.org>
2016-11-10 12:53:23 -0600

    update tests configuration

--- tests/intern.ts
+++ tests/intern.ts
@@ -1,41 +1,40 @@
-define({
-	capabilities: {
-		'selenium-version': '2.43.0',
-		'idle-timeout': 30
-	},
+export const capabilities = {
+	'selenium-version': '2.43.0',
+	'idle-timeout': 30
+};
 
-	environments: [
-		{ browserName: 'microsoftedge', fixSessionCapabilities: false },
-		{ browserName: 'internet explorer', version: '11', platform: 'WIN8', fixSessionCapabilities: false },
-		{ browserName: 'internet explorer', version: '10', platform: 'WIN8', fixSessionCapabilities: false },
-		{ browserName: 'internet explorer', version: '9', platform: 'WINDOWS', fixSessionCapabilities: false },
-		{ browserName: 'firefox', version: '33', platform: [ 'WINDOWS', 'MAC' ], fixSessionCapabilities: false },
-		{ browserName: 'chrome', version: '38', platform: [ 'WINDOWS', 'MAC' ], fixSessionCapabilities: false },
-		{ browserName: 'safari', version: '9', platform: 'MAC', fixSessionCapabilities: false }
-	],
+export const environments = [
+	{ browserName: 'microsoftedge', fixSessionCapabilities: false },
+	{ browserName: 'internet explorer', version: '11', platform: 'WIN8', fixSessionCapabilities: false },
+	{ browserName: 'internet explorer', version: '10', platform: 'WIN8', fixSessionCapabilities: false },
+	{ browserName: 'internet explorer', version: '9', platform: 'WINDOWS', fixSessionCapabilities: false },
+	{ browserName: 'firefox', version: '33', platform: [ 'WINDOWS', 'MAC' ], fixSessionCapabilities: false },
+	{ browserName: 'chrome', version: '38', platform: [ 'WINDOWS', 'MAC' ], fixSessionCapabilities: false },
+	{ browserName: 'safari', version: '9', platform: 'MAC', fixSessionCapabilities: false }
+];
 
-	maxConcurrency: 2,
-	tunnel: 'BrowserStackTunnel',
+export const maxConcurrency = 2;
+export const tunnel = 'BrowserStackTunnel';
 
-	loaderOptions: {
-		packages: [
-			{ name: 'leadfoot', location: '.' }
-		]
-	},
+export const loaderOptions = {
+	packages: [
+		{ name: 'leadfoot', location: 'dist' },
+		{ name: 'dojo', location: 'node_modules/dojo'}
+	]
+};
 
-	suites: [
-		'dojo/has!host-node?leadfoot/tests/unit/lib/util',
-		'dojo/has!host-node?leadfoot/tests/unit/compat'
-	],
+export const suites = [
+	'dojo/has!host-node?leadfoot/tests/unit/lib/util',
+	'dojo/has!host-node?leadfoot/tests/unit/compat'
+];
 
-	functionalSuites: [
-		'leadfoot/tests/functional/helpers/pollUntil',
-		'leadfoot/tests/functional/Server',
-		'leadfoot/tests/functional/Session',
-		'leadfoot/tests/functional/Element',
-		'leadfoot/tests/functional/Command',
-		'leadfoot/tests/functional/compat'
-	],
+export const functionalSuites = [
+	'leadfoot/tests/functional/helpers/pollUntil',
+	'leadfoot/tests/functional/Server',
+	'leadfoot/tests/functional/Session',
+	'leadfoot/tests/functional/Element',
+	'leadfoot/tests/functional/Command',
+	'leadfoot/tests/functional/compat'
+];
 
-	excludeInstrumentation: /^(?:tests|node_modules)\//
-});
+export const excludeInstrumentation = /^(?:tests|node_modules)\//;
--- tests/unit/lib/util.ts
+++ tests/unit/lib/util.ts
@@ -1,52 +1,48 @@
-define([
-	'intern!object',
-	'intern/chai!assert',
-	'intern/dojo/node!../../../lib/util'
-], function (registerSuite, assert, util) {
-	registerSuite({
-		name: 'lib/leadfoot/util',
+import registerSuite = require('intern!object');
+import * as assert from 'intern/chai!assert';
+import * as util from '../../../src/lib/util';
 
-		'.sleep': function () {
-			var startTime = Date.now();
-			return util.sleep(250).then(function () {
-				assert.closeTo(Date.now() - startTime, 250, 50);
-			});
-		},
+registerSuite({
+	name: 'lib/leadfoot/util',
 
-		'.sleep canceler': function () {
-			var startTime = Date.now();
-			var sleep = util.sleep(10000);
-			sleep.cancel();
-			return sleep.then(function () {
-				throw new Error('Sleep should have been cancelled');
-			}, function (error) {
-				assert.operator(Date.now() - startTime, '<', 500);
-				assert.strictEqual(error.name, 'CancelError');
-			});
-		},
+	'.sleep'() {
+		const startTime = Date.now();
+		return util.sleep(250).then(function () {
+			assert.closeTo(Date.now() - startTime, 250, 50);
+		});
+	},
 
-		'.forCommand': function () {
-			var commandFn = util.forCommand(function () {}, {
-				createsContext: false,
-				usesElement: true
-			});
-			assert.isFalse(commandFn.createsContext);
-			assert.isTrue(commandFn.usesElement);
-		},
+	'.sleep canceler'() {
+		const startTime = Date.now();
+		const sleep = util.sleep(10000);
+		sleep.cancel();
+		return sleep.then(function () {
+			throw new Error('Sleep should have been cancelled');
+		}, function (error) {
+			assert.operator(Date.now() - startTime, '<', 500);
+			assert.strictEqual(error.name, 'CancelError');
+		});
+	},
 
-		'.toExecuteString string': function () {
-			var script = util.toExecuteString('return a;');
-			assert.strictEqual(script, 'return a;');
-		},
+	'.forCommand'() {
+		const commandFn = util.forCommand(function () {}, {
+			createsContext: false,
+			usesElement: true
+		});
+		assert.isFalse(commandFn.createsContext);
+		assert.isTrue(commandFn.usesElement);
+	},
 
-		'.toExecuteString function': function () {
-			/*jshint camelcase:false */
-			/*global __cov_abcdef:false, a:false */
-			var script = util.toExecuteString(function () {
-				__cov_abcdef++;
-				return a;
-			});
-			assert.match(script, /^return \(function \(\) \{\s*return a;\s*\}\)\.apply\(this, arguments\);$/);
-		}
-	});
+	'.toExecuteString string'() {
+		const script = util.toExecuteString('return a;');
+		assert.strictEqual(script, 'return a;');
+	},
+
+	'.toExecuteString function'() {
+		const script = util.toExecuteString(function () {
+			__cov_abcdef++;
+			return a;
+		});
+		assert.match(script, /^return \(function \(\) \{\s*return a;\s*\}\)\.apply\(this, arguments\);$/);
+	}
 });
