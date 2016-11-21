f05545b5d2054d65833369fe41e252ff315e8384
Nick Nisi <nick@nisi.org>
2016-11-21 16:48:50 -0600

    add index file, exporting modules for CJS

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as Server } from './Server';
+export { default as Session} from './Session';
+export { default as Command } from './Command';
+export { default as keys } from './keys';
+export { default as pollUntil } from './helpers/pollUntil';
