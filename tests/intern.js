4835b1dbffb8e247f24b1d173b31434f1b3fbee0
Jason Cheatham <j.cheatham@gmail.com>
2017-05-24 15:36:25 -0400

    Add testing profiles for other services
    Can now run `npm test service={sauce,testingbot}` to use something other
    than BrowserStack

--- tests/intern.js
+++ tests/intern.js
@@ -1,40 +1,73 @@
-define({
-	capabilities: {
-		'idle-timeout': 30
-	},
+define([ 'intern' ], function (intern) {
+	var config = {
+		capabilities: {
+			'idle-timeout': 30
+		},
 
-	environments: [
-		{ browserName: 'microsoftedge', fixSessionCapabilities: false },
-		{ browserName: 'internet explorer', version: '11', platform: 'WIN8', fixSessionCapabilities: false },
-		{ browserName: 'internet explorer', version: '10', platform: 'WIN8', fixSessionCapabilities: false },
-		{ browserName: 'internet explorer', version: '9', platform: 'WINDOWS', fixSessionCapabilities: false },
-		{ browserName: 'firefox', version: [ '33', '49' ], platform: [ 'WINDOWS', 'MAC' ], fixSessionCapabilities: false },
-		{ browserName: 'chrome', version: [ '38', '52' ], platform: [ 'WINDOWS', 'MAC' ], fixSessionCapabilities: false },
-		{ browserName: 'safari', version: [ '9', '10' ], platform: 'MAC', fixSessionCapabilities: false }
-	],
+		environments: [
+			{ browserName: 'microsoftedge', fixSessionCapabilities: false },
+			{ browserName: 'internet explorer', version: '11', fixSessionCapabilities: false },
+			{ browserName: 'internet explorer', version: '10', fixSessionCapabilities: false },
+			{ browserName: 'internet explorer', version: '9', fixSessionCapabilities: false },
+			{ browserName: 'firefox', version: [ '33', '53' ], platform: [ 'WINDOWS', 'MAC' ], fixSessionCapabilities: false },
+			{ browserName: 'safari', version: [ '9', '10' ], fixSessionCapabilities: false }
+		],
 
-	maxConcurrency: 2,
-	tunnel: 'BrowserStackTunnel',
+		maxConcurrency: 2,
+		tunnel: 'BrowserStackTunnel',
 
-	loaderOptions: {
-		packages: [
-			{ name: 'leadfoot', location: '.' }
-		]
-	},
+		loaderOptions: {
+			packages: [
+				{ name: 'leadfoot', location: '.' }
+			]
+		},
 
-	suites: [
-		'dojo/has!host-node?leadfoot/tests/unit/lib/util',
-		'dojo/has!host-node?leadfoot/tests/unit/compat'
-	],
+		suites: [
+			'dojo/has!host-node?leadfoot/tests/unit/lib/util',
+			'dojo/has!host-node?leadfoot/tests/unit/compat'
+		],
 
-	functionalSuites: [
-		'leadfoot/tests/functional/helpers/pollUntil',
-		'leadfoot/tests/functional/Server',
-		'leadfoot/tests/functional/Session',
-		'leadfoot/tests/functional/Element',
-		'leadfoot/tests/functional/Command',
-		'leadfoot/tests/functional/compat'
-	],
+		functionalSuites: [
+			'leadfoot/tests/functional/helpers/pollUntil',
+			'leadfoot/tests/functional/Server',
+			'leadfoot/tests/functional/Session',
+			'leadfoot/tests/functional/Element',
+			'leadfoot/tests/functional/Command',
+			'leadfoot/tests/functional/compat'
+		],
 
-	excludeInstrumentation: /^(?:tests|node_modules)\//
+		excludeInstrumentation: /^(?:tests|node_modules)\//
+	};
+
+	if (intern.args.service === 'sauce') {
+		var platforms = {
+			WINDOWS: 'Windows 10',
+			MAC: 'OS X 10.12'
+		};
+		config.environments.forEach(function (environment) {
+			if (environment.platform) {
+				environment.platform = environment.platform.map(function (platform) {
+					return platforms[platform] || platform;
+				});
+			}
+		});
+		config.tunnel = 'SauceLabsTunnel';
+	}
+
+	if (intern.args.service === 'testingbot') {
+		var platforms = {
+			WINDOWS: 'WIN10',
+			MAC: 'SIERRA'
+		};
+		config.environments.forEach(function (environment) {
+			if (environment.platform) {
+				environment.platform = environment.platform.map(function (platform) {
+					return platforms[platform] || platform;
+				});
+			}
+		});
+		config.tunnel = 'TestingBotTunnel';
+	}
+
+	return config;
 });
